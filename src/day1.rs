f868429abcd4370554c18c670741a3959c128597
Maksym Shcherban <peregrin@ukr.net>
2024-12-02 08:58:55 +0100

    Day1 test

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,11 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
+#[aoc(day1, part1)]
 pub fn part1(input: &str) -> u32 {
   return 7
 }
 
+#[aoc(day1, part2)]
 pub fn part2(input: &str) -> u32 {
   return 30
 }
